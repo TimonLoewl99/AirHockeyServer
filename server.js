f4063fb079a833aadb3c991bee12326583b19890
Timon Löwl <timonlowl@Timons-MacBook-Pro.local>
2022-07-19 16:59:47 +0200

    Server update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://ohmline.herokuapp.com/",
+    origin: "https://ohmline.herokuapp.com",
     methods: ["GET", "POST"],
   },
 });
